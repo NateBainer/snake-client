0d7bc3ef9859d4e199cb35bdbd59f216b96babe9
Nathan Bain <nate.bain9@gmail.com>
2022-05-05 10:04:39 -0400

    final copy

--- client.js
+++ client.js
@@ -1,31 +1,36 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    // IP address here,
-    port: 50541// PORT number here,
-  });
+const connect = () => {
+  const name = 'Name: CYN';
+  const up = 'Move: up';
+  const down = 'Move: down';
+  const left = 'Move: left';
+  const right = 'Move: right';
 
+  const conn = net.createConnection({
+    host: IP,// IP address here,
+    port: PORT // PORT number here,
+  });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-  
+
+  conn.write(`${name}`);
+
   conn.on('data', (data) => {
-    console.log(data);
+    console.log('received: ' + data);
   });
 
   conn.on('connect', () => {
-    console.log('YO WE CONNECT');
-    conn.write('Name: McB');
-    // conn.write('Move: down');
+    console.log("you're connected!!");
   });
-
-  
-
   return conn;
 };
+
+
+module.exports = connect;
 
 console.log("Connecting ...");
 
---
+++ constant.js
@@ -0,0 +1,9 @@
+const IP = '10.0.0.248';
+const PORT = 50541;
+const MOVE_UP_KEY = "\u007a";
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP_KEY
+};
